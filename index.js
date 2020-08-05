17616a99c5d6bea5d8df7da036c9634ae12d9c00
Tanner Leigh <tannerhleigh@gmail.com>
2020-08-04 21:36:10 -0700

    [html and js] finished implementing html files to node file

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express')
-
+const path = require('path')
 const app = express()
 
+
 app.get('/', (req, res) => {
-    res.send('This is the Home Page')
+    res.sendFile(path.join(__dirname, './HTML', 'index.html'))
 })
 
 app.get('/about', (req, res) => {
-    res.send('Some stuff about me will go here')
+    res.sendFile(path.join(__dirname, './HTML', 'about.html'))
 })
 
+
 app.get('/blog', (req, res) => {
-    res.send('A directory of all my blog posts will go here.')
+    res.sendFile(path.join(__dirname, './HTML', 'blog-directory.html'))
 })
 
 app.listen(8000)

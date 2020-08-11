4c8c1a5dad5dceb177306994c238015c3d48551b
Tanner Leigh <tannerhleigh@gmail.com>
2020-08-11 16:45:59 -0700

    [finished] last commit

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const express = require('express')
-const path = require('path')
 const app = express()
 
+//tell express that we're going to use
+//ejs as the view engine
+app.set('view engine', 'ejs')
+
+//home route!
+// app.get('/', (req, res) => {
+//     res.render('index') //express will look inside views folder by default for an ejs file with this name
+// })
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './HTML', 'index.html'))
+    res.render('index', {name: "Sterling Archer", age: 35})
+    // res.sendFile(path.join(__dirname, './views', 'index.html'))
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, './HTML', 'about.html'))
+    res.render('about', {fave: ['music', 13, 'SEI']})
+    // res.sendFile(path.join(__dirname, './views', 'about.html'))
 })
 
-
 app.get('/blog', (req, res) => {
-    res.sendFile(path.join(__dirname, './HTML', 'blog-directory.html'))
+    res.render('blog-directory')
+    // res.sendFile(path.join(__dirname, './views', 'blog-directory.html'))
 })
 
 app.listen(8000)
